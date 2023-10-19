b63144aacb044ecda3f44681562b0d1b464b2111
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-10-19 09:55:57 -0700

    changes in files

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,18 @@ module.exports = {
   plugins: [],
 }
 
+module.exports = {
+
+  plugins: [
+      require('flowbite/plugin')
+  ]
+
+}
+
+module.exports = {
+
+  content: [
+      "./node_modules/flowbite/**/*.js"
+  ]
+
+}
